c7bde09d171a356cb6da27cb0fbce8195e93f4f6
SwayamWakodikar <swayam.w06@gmail.com>
2025-10-27 23:16:58 +0530

    added profile pic

---
+++ client/src/assets/assets.js
@@ -0,0 +1,8 @@
+import profile_pic from "./Swayam_Pic.jpg"
+const assets = [
+  profile_pic
+  // Add more assets here as needed, e.g.:
+  // { name: "logo", src: logo }
+];
+
+export default assets;
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const Profile = () => {
   return (
-  <div className="w-screen h-96">
-  <div className="text-center text-4xl font-bold bg-linear-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent text-shadow-lg">
-    Swayam Wakodikar
+    
+  <div className="w-screen h-auto">
+  <div className="text-center text-4xl font-bold bg-linear-to-r from-indigo-500 to-blue-500 bg-clip-text text-shadow-lg flex">
+    <div className=" self-start ml-4">
+      <img 
+        src={assets[0]} 
+        alt="Profile" 
+        className=" h-72 w-72 rounded-full object-cover border-2 border-black"
+      />
+      <div className='text-black font-bold pb-2'>
+        Student | Tech Enthusiast
+        <br />
+        Developer | Electronics Geek
+    </div>
+    <div>
+      
+    </div>
+    </div>
+    
   </div>
 </div>
 
