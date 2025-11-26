0c71a47dbdd808d23101f2fb3fa0f19d10cfb642
SwayamWakodikar <swayam.w06@gmail.com>
2025-11-26 19:36:42 +0530

    Created userSchema for contact Me

--- server/src/model/userModel.js
+++ server/src/model/userModel.js
@@ -1,1 +1,19 @@
 import mongoose from 'mongoose';
+
+const userSchema=new mongoose.Schema({
+    viewer:{
+        name:{
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        message:{
+            type: String,
+            required: true,
+        }
+
+    }
+})
