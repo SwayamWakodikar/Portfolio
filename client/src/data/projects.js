9a9d148a4dca8aa0041c2c21078908b87032e6a0
SwayamWakodikar <swayam.w06@gmail.com>
2026-01-12 23:00:43 +0530

    reduced the projects category

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -16,7 +16,7 @@ export const projects = [
       title: "Auth WebApp",
       description: "A Project Integrating JWT, OAuth on Learning Purpose.",
       tech: ["React", "MongoDB", "JWT", "OAuth", "Express", "Node.js", "Bcrypt"],
-      category: "Auth & Security",
+      category: "Full Stack",
       image: "https://images.unsplash.com/photo-1614064641938-3bbee52942c7?auto=format&fit=crop&q=80&w=600", 
       github: "https://github.com/SwayamWakodikar/Authentication-WebApp.git",
       demo: "#"
@@ -26,7 +26,7 @@ export const projects = [
       title: "Vplace: AI-Powered Career Suite",
       description: "Developed for TechSprint'25, a GDG on-campus hackathon. An AI-driven platform for resume generation and ATS optimization, where I spearheaded the frontend and database architecture.",
       tech: ["Next.js 15", "TypeScript", "Tailwind CSS 4", "Firebase Firestore", "NextAuth.js", "OpenRouter API", "ShadcnUI", "Motion"],
-      category: "AI & Career Development",
+      category: "AI Projects",
       image: vplaceImg,
       github: "https://github.com/SwayamWakodikar/Vplace.git",
       demo: "https://vplace.vercel.app"
